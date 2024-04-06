0353b4bf1b5936d5dd33733c536b7999c89007e0
blandskron <bastianlandskronfreelancer@gmail.com>
2024-04-06 18:10:36 -0300

    formulario

--- forms/formsapp/views.py
+++ forms/formsapp/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
 from .forms import QuestionForm
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('formsapp/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+def detail(request, question_id):
+    return HttpResponse("Estas buscando una pregunta %s." % question_id)
+
+def results(request, question_id):
+    response = "Estas buscando el resultado de una pregunta %s."
+    return HttpResponse(response % question_id)
+ 
+def vote(request, question_id):
+    return HttpResponse("tu estas votando por una pregunta %s." % question_id)
 
 def success(request):
     return render(request, 'formsapp/success.html')
@@ -15,4 +37,5 @@ def question(request):
         form = QuestionForm()
     return render(request, 'formsapp/form.html', {"form": form})
 
-
+def back_to_home(request):
+    return redirect('index')
